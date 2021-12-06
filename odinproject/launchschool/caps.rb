8a8c13c3d100784d66df5e3a5ea30b30b84da8e1
Jason Wesson <jwesson@codeforamerica.org@Jasons-MacBook-Pro.local>
2021-12-06 10:07:35 -0600

    Insert prompts for both caps and range.rb

--- odinproject/launchschool/caps.rb
+++ odinproject/launchschool/caps.rb
@@ -1,3 +1,5 @@
+# Write a method that takes a string as an argument.
+# The method should return a new, all-caps version of the string, only if the string is longer than 10 characters.
 def caps (string)
   if string.length >= 10
     string = string.upcase
--- odinproject/launchschool/range.rb
+++ odinproject/launchschool/range.rb
@@ -1,3 +1,5 @@
+# Write a program that takes a number from the user between 0 and 100
+# and reports back whether the number is between 0 and 50, 51 and 100, or above 100.
 def range(number)
   puts "pick a number between 0 and 100"
   number = gets.chomp.to_i
